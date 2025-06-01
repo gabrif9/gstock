352e27c0a372175654ff95540e071bd650c7fa28
gabrif9 <gabrieleferrigno95@gmail.com>
2025-06-01 17:26:51 +0200

    fix: change tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -54,6 +54,7 @@ const themes = {
 };
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -61,12 +62,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      }
     },
   },
-  plugins: [],
+  plugins: [createThemes(themes)],
 };
 export default config;
