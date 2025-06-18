0f37d6aeab375465bdbf382b039377f18ebba534
gabrif9 <gabrieleferrigno95@gmail.com>
2025-06-18 19:16:11 +0200

    feat: add image configuration in next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s3-inventorymanagement-gstock.s3.us-east-1.amazonaws.com",
+                port: "",
+                pathname: "/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
