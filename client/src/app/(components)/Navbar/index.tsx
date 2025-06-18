faec8ed052908a8cc90cf6381e75d4ccc5eb50d2
gabrif9 <gabrieleferrigno95@gmail.com>
2025-06-18 19:19:01 +0200

    feat: add profile image in navbar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -3,6 +3,7 @@
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 import { Bell, Menu, Moon, Settings, Sun } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
@@ -70,9 +71,13 @@ const Navbar = () => {
             </div>
             <hr className='w-0 h-7 border border-solid border-l border-gray-300 mx-3' />
             <div className='flex items-center gap-3 cursor-pointer'>
-              <div className='w-9 h-9'>
-                image
-              </div>
+                <Image
+                  src="s3-inventorymanagement-gstock.s3.us-east-1.amazonaws.com/profile.png"
+                  alt="Profile"
+                  width={50}
+                  height={50}
+                  className='rounded-full h-full object-cover'
+                  />
               <div>
                 <span className='font-semibold'>Gabrif9</span>
               </div>
